aec0aac89a43140e640f1c50a2c78cde13256b23
Phat179 <thephat1709@gmail.com>
2024-06-20 22:10:32 +0700

    Re-design project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
-const showPopupBtn = document.querySelector(".login-btn");
-const formPopup = document.querySelector(".form-popup");
-const hidePopupBtn = document.querySelector(".form-popup .close-btn"); 
-const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
-showPopupBtn.addEventListener("click", () => {
-    document.body.classList.toggle("show-popup");
-});
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
 
-hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
-
-loginSignupLink.forEach(link => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
-    });
-});
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
